d91c76bb9bbc1515ca4677838e6b2f37b1baf40e
DobrokhvalovRA <DobrokhvalovRA@icloud.com>
2025-01-15 09:31:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void checkPhone(ArrayList phoneNumbers) throws NameExeption {
+            if (phoneNumbers == null) throw new NameExeption("Данный абонент не найден!");
+    }
+
+
+
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.setPerson("Иванов", "+79179875000");
@@ -7,6 +16,14 @@ public class Main {
         phoneBook.setPerson("Иванов", "+79179875003");
         phoneBook.setPerson("Иванов", "+79179875004");
 
-        System.out.println(phoneBook.getPhoneNumber("Иванов"));
+        String checkName = "Сидоров1";
+
+        try {
+            checkPhone(phoneBook.getPhoneNumber(checkName));
+            System.out.println("Абонент " + checkName + " имеет следующий телефонный номер: " + phoneBook.getPhoneNumber(checkName));
+        } catch (NameExeption e) {
+            System.out.println("Ошибочка: " + e.getMessage());
+        }
+
     }
 }
