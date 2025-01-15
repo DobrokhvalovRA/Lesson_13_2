bbd506e2dcbd9a18d9b2c6f81df484174c00dc6d
DobrokhvalovRA <DobrokhvalovRA@icloud.com>
2025-01-15 09:52:52 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,35 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void checkPhone(ArrayList phoneNumbers) throws NameExeption {
-            if (phoneNumbers == null) throw new NameExeption("Данный абонент не найден!");
+            if (phoneNumbers == null) throw new NameExeption("не найден!");
     }
 
 
 
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.setPerson("Иванов", "+79179875000");
-        phoneBook.setPerson("Петров", "+79179875001");
-        phoneBook.setPerson("Сидоров", "+79179875002");
-        phoneBook.setPerson("Иванов", "+79179875003");
-        phoneBook.setPerson("Иванов", "+79179875004");
+        //Добавляем абонентов
+        phoneBook.addPerson("Иванов", "+79179875000");
+        phoneBook.addPerson("Петров", "+79172222222");
+        phoneBook.addPerson("Сидоров", "+79179875002");
+        phoneBook.addPerson("Иванов", "+79179875003");
+        phoneBook.addPerson("Иванов", "+79179875004");
 
-        String checkName = "Сидоров1";
 
+        //Вводим фамилию для поиска телефонного номера
+        System.out.println("Введите фамилию абонента:");
+        Scanner console = new Scanner(System.in);
+        String checkName = console.nextLine();
+
+        //Проыеряем фамилию и вывобим номер
         try {
             checkPhone(phoneBook.getPhoneNumber(checkName));
             System.out.println("Абонент " + checkName + " имеет следующий телефонный номер: " + phoneBook.getPhoneNumber(checkName));
         } catch (NameExeption e) {
-            System.out.println("Ошибочка: " + e.getMessage());
+            System.out.println("Абонент с фамилией " + checkName + " - " + e.getMessage());
         }
 
     }
